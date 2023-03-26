ad01470f5ac478e623bcd8c2c4cb21dc73bde0a5
jwlankford <jwlankford@gmail.com>
2023-03-26 13:11:53 -0400

    3 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,68 @@
 import * as React from 'react';
+import { useState } from 'react';
 import './style.css';
 
+import form from '/components/form/';
+
 export default function App() {
+  const [showSideBar, SetShowSider] = useState(false);
+
   return (
     <div>
-      <h1>Hello!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <div className="container mx-auto">
+        <div className="flex flex-row flex-wrap py-4">
+          <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
+            <div className="sticky top-0 p-4 w-full bg-black">
+              <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                <i className="bi bi-house-door-fill"></i>
+                <span className="text-[15px] ml-4 text-gray-200 font-bold">
+                  Home
+                </span>
+              </div>
+              <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                <i className="bi bi-bookmark-fill"></i>
+                <span className="text-[15px] ml-4 text-gray-200 font-bold">
+                  Bookmark
+                </span>
+              </div>
+              <div className="my-4 bg-gray-600 h-[1px]"></div>
+              <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                <i className="bi bi-chat-left-text-fill"></i>
+                <div className="flex justify-between w-full items-center">
+                  <span className="text-[15px] ml-4 text-gray-200 font-bold">
+                    Chatbox
+                  </span>
+                  <span className="text-sm rotate-180" id="arrow">
+                    <i className="bi bi-chevron-down"></i>
+                  </span>
+                </div>
+              </div>
+              <div
+                className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold"
+                id="submenu"
+              >
+                <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+                  Social
+                </h1>
+                <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+                  Personal
+                </h1>
+                <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+                  Friends
+                </h1>
+              </div>
+              <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                <i className="bi bi-box-arrow-in-right"></i>
+                <span className="text-[15px] ml-4 text-gray-200 font-bold">
+                  Logout
+                </span>
+              </div>
+            </div>
+          </aside>
+          <main role="main" className="sm:w-2/3 md:w-1/2 pt-1 px-4"></main>
+        </div>
+      </div>
+      <footer className="mt-auto">...</footer>
     </div>
   );
 }
